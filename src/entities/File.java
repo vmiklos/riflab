353bfc22778ebabfc28eef249064bb351a102dd1
Miklos Vajna <vmiklos@frugalware.org>
2011-03-24 13:47:27 +0100

    externalizable impl

--- src/entities/File.java
+++ src/entities/File.java
@@ -1,6 +1,11 @@
 package entities;
 
-public class File {
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class File implements Externalizable{
 	private String name;
 	private String content;
 	private Integer quality;
@@ -22,5 +27,18 @@ public class File {
 	}
 	public void setQuality(Integer quality) {
 		this.quality = quality;
+	}
+	@Override
+	public void readExternal(ObjectInput in) throws IOException,
+			ClassNotFoundException {
+		name = in.readUTF();
+		content = in.readUTF();
+		quality = in.readInt();
+	}
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.writeUTF(name);
+		out.writeUTF(content);
+		out.writeInt(quality);
 	}
 }
