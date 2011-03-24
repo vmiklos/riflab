e8dda711fafa6827e6952927f6887ce4dac3f63e
Miklos Vajna <vmiklos@frugalware.org>
2011-03-24 13:47:27 +0100

    externalizable impl

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,9 +1,17 @@
 package entities;
 
+import java.io.Externalizable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Product {
+public class Product implements Externalizable {
 	private int id;
 	private boolean verified;
 	private boolean valid;
@@ -74,5 +82,106 @@ public class Product {
 	}
 	public void setSrs(SRS srs) {
 		this.srs = srs;
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException,
+			ClassNotFoundException {
+		id = in.readInt();
+		verified = in.readBoolean();
+		valid = in.readBoolean();
+		int size = in.readInt();
+		sourceFiles = new LinkedList<File>();
+		for (int i = 0; i < size; i++) {
+			sourceFiles.add((File)in.readObject());
+		}
+		size = in.readInt();
+		testSuite = new LinkedList<File>();
+		for (int i = 0; i < size; i++) {
+			testSuite.add((File)in.readObject());
+		}
+		size = in.readInt();
+		documentation = new LinkedList<File>();
+		for (int i = 0; i < size; i++) {
+			documentation.add((File)in.readObject());
+		}
+		size = in.readInt();
+		architecture = new LinkedList<String>();
+		for (int i = 0; i < size; i++) {
+			architecture.add(in.readUTF());
+		}
+		srs = (SRS)in.readObject();
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.writeInt(id);
+		out.writeBoolean(verified);
+		out.writeBoolean(valid);
+		out.writeInt(sourceFiles.size());
+		for(File f : sourceFiles) {
+			out.writeObject(f);
+		}
+		out.writeInt(testSuite.size());
+		for(File f : testSuite) {
+			out.writeObject(f);
+		}
+		out.writeInt(documentation.size());
+		for(File f : documentation) {
+			out.writeObject(f);
+		}
+		out.writeInt(architecture.size());
+		for(String s : architecture) {
+			out.writeUTF(s);
+		}
+		out.writeObject(srs);
+	}
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+        List<String> requirements = new LinkedList<String>();
+        requirements.add("elso kovetelmeny");
+        requirements.add("masodik kovetelmeny");
+        Product product = new Product(1);
+        product.getSrs().setRequirements(requirements);
+        product.getSrs().setConsistent(true);
+        List<String> architecture = new LinkedList<String>();
+        architecture.add("model");
+        architecture.add("view");
+        architecture.add("controller");
+        product.setArchitecture(architecture);
+        List<File> sourceFiles = new LinkedList<File>();
+        File file = new File();
+        file.setContent("public static void main()");
+        file.setName("main.java");
+        file.setQuality(100);
+        sourceFiles.add(file);
+        product.setSourceFiles(sourceFiles);
+        List<File> documentation = new LinkedList<File>();
+        File file2 = new File();
+        file2.setContent("doc content");
+        file2.setName("doc.txt");
+        file2.setQuality(100);
+        documentation.add(file);
+        product.setDocumentation(documentation);
+        List<File> testSuite = new LinkedList<File>();
+        File file3 = new File();
+        file3.setContent("public static void test()");
+        file3.setName("test.java");
+        file3.setQuality(100);
+        testSuite.add(file);
+        product.setTestSuite(testSuite);
+        product.setValid(true);
+        product.setVerified(true);
+        
+		FileOutputStream fos = new FileOutputStream("/tmp/product");
+		ObjectOutputStream outStream = new ObjectOutputStream(fos);
+		product.writeExternal(outStream);
+		outStream.close();
+		
+		FileInputStream fis = new FileInputStream("/tmp/product");
+		ObjectInputStream inStream = new ObjectInputStream(fis);
+		Product p2 = new Product(0);
+		p2.readExternal(inStream);
+		System.out.println(p2);
 	}
 }
--- src/entities/SRS.java
+++ src/entities/SRS.java
@@ -1,9 +1,13 @@
 package entities;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SRS {
+public class SRS  implements Externalizable {
 	private List<String> requirements;
 	private boolean consistent;
 
@@ -21,5 +25,23 @@ public class SRS {
 	}
 	public void setConsistent(boolean consistent) {
 		this.consistent = consistent;
+	}
+	@Override
+	public void readExternal(ObjectInput in) throws IOException,
+			ClassNotFoundException {
+		consistent = in.readBoolean();
+		int size = in.readInt();
+		requirements = new LinkedList<String>();
+		for (int i = 0; i < size; i++) {
+			requirements.add(in.readUTF());
+		}
+	}
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.writeBoolean(consistent);
+		out.writeInt(requirements.size());
+		for(String s : requirements) {
+			out.writeUTF(s);
+		}
 	}
 }
